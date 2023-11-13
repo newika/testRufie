5c02d4e31d1e78033f2bcc1079720c505e7f4f9f
newika <chervonkina565@gmail.com>
2023-11-13 20:41:49 +0300

    v1.3

--- second_win.py
+++ second_win.py
@@ -1,1 +1,10 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLayout, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+
+if 
+    app = QApplication([])
+    main_win = QWidget()
+    main_win.show()
+    main_win.setWindowTitle('Тест Руфье')
+    main_win.move(900, 700)
+    main_win.resize(400, 200)
